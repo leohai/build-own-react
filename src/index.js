3af35ab9ff9621c10159e63efa9cdcc9168f5e0d
leohai <448456376@qq.com>
2023-06-19 23:40:34 +0800

    feat: Step VI, Reconciliation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,61 @@
 let nextUnitOfWork = null
 let wipRoot = null // 根fiber树
+let currentRoot = null
+let deletions = null // 标记需要删除的节点
+const isProperty = (key) => key !== 'children'
+const isEvent = (key) => key.startsWith('on')
+const isGone = (pre, next) => key => !(key in next)
+const isNew = (pre, next) => key => next[key] != pre[key]
 
 function commitRoot() {
+  deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  currentRoot = wipRoot
   wipRoot = null
 }
 
 function commitWork(fiber) {
   if(!fiber) return;
+
   const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
+  if(fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
+    domParent.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+  } else if (fiber.effectTag === 'DELETIONS' && fiber.dom != null) {
+    domParent.removeChild(fiber.dom)
+  } 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
 }
+function updateDom(dom, prevProps, nextProps) {
+  // remove , update listener
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.removeListener(eventType, prevProps[name])
+    })
 
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((key) => (dom[key] = ''))
+
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((key) => (dom[key] = nextProps[key]))
+  //  add
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter((key) => isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, nextProps[name])
+    })
+}
 function workLoop(deadline) {
   let shouldYeild = false
   while(nextUnitOfWork && !shouldYeild) {
@@ -31,30 +73,10 @@ function performUnitOfWork (fiber) {
   if(!fiber.dom) {
     fiber.dom = createDom(fiber)
   }
-  // if(fiber.parent) {
-  //   fiber.parent.dom.append(fiber.dom)
-  // }
+  // diff current fiber wip fiber 树
   const elements = fiber.props.children
-  let index = 0
-  let preSibling = null
-  // generate child fiber, add parent ,sibling
-  while (index < elements.length) {
-    const element = elements[index]
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    }
-    if (index === 0) {
-      fiber.child = newFiber
-    } else {
-      preSibling.sibling = fiber
-    }
-    preSibling = fiber
-    index ++;
-  }
-  
+  reconcilerChildren(fiber, elements)
+
   // return fiber
   if(fiber.child) {
     return fiber.child
@@ -70,32 +92,74 @@ function performUnitOfWork (fiber) {
 }
 requestIdleCallback(workLoop)
 
-function createDom(element) {
+function reconcilerChildren(wipFiber, elements) {
+  let index = 0
+  const oldFiber = wipFiber.alternate && wipFiber.alternate.child
+  let preSibling = null
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index]
+    const sameType = oldFiber && element && oldFiber.type === element.type
+    let newFiber = null
+    if (sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        parent: wipFiber,
+        dom: oldFiber.dom,
+        alternate: oldFiber,
+        effectTag: 'UPDATE',
+      }
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        parent: wipFiber,
+        dom: null,
+        alternate: null,
+        effectTag: 'PLACEMENT',
+      }
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = 'DELETION'
+      deletions.push(oldFiber)
+    }
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+    // generate child fiber, add parent ,sibling
+    if (index === 0) {
+      wipFiber.child = newFiber
+    } else {
+      preSibling.sibling = wipFiber
+    }
+    preSibling = wipFiber
+    index++
+  }
+}
+function createDom(fiber) {
   // 1创建dom
   const dom =
-    element.type == 'TEXT_ELEMENT'
+    fiber.type == 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type)
+      : document.createElement(fiber.type)
 
   // 添加dom属性
-  const isProperty = (key) => key !== 'children'
 
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name]
-    })
+  updateDom(dom, {}, fiber.props)
 
   return dom
 }
 
 function render(element, container) {
-  wipRoot= {
+  wipRoot = {
     props: {
       children: [element],
     },
     dom: container,
+    alternate: currentRoot,
   }
+  deletions = []
   nextUnitOfWork = wipRoot
 }
 
@@ -137,7 +201,7 @@ function Counter() {
     </h1>
   )
 }
-const h1 = <h2 className="ddb">adadada</h2>
+const h1 = <h2 className="ddb" onClick={() => alert(2)}>adadada</h2>
 // const element = <Counter />
 const element = h1
 console.log(h1)
